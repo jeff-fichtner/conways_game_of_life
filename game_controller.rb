3275bd6f95296896f514772a40b65084fd816433
Jeff Fichtner <jeff.fichtner@gmail.com>
2016-09-05 17:16:26 -0500

    Add glider test method, include reset screen

--- game_controller.rb
+++ game_controller.rb
@@ -1,21 +1,31 @@
 require_relative 'board_generator'
 require_relative 'game'
 require_relative 'game_view'
+require_relative 'reset_screen'
 
 class GameController
 
   def self.run
+
+    # initialize animation
     generation_count = 0
-    generations = 1000
+    generations = 500
     width = 20
     height = 20
-    game = Game.new(BoardGenerator.generate(20, 20))
+    game = Game.new(BoardGenerator.generate(60, 20))
 
-    # alter board in some way
+    # alter the board
+    game.board = add_glider(game.board)
+    reset_screen
+    GameView.display_board(game.board)
+    sleep(0.5)
 
+    # start evolution
     while generation_count < generations
       game.board = evolve(game)
+      reset_screen
       GameView.display_board(game.board)
+      sleep(0.2)
       generation_count += 1
     end
 
@@ -23,6 +33,14 @@ class GameController
 
 
   private
+
+  def self.add_glider(board)
+    board[3][2] = true
+    board[3][3] = true
+    board[3][4] = true
+    board[2][4] = true
+    board[1][3] = true
+  end
 
   def self.evolve(game)
     current_board = game.board
