6ce2c9a1dfadb88d31728fe4aef9285085a083c4
Jeff Fichtner <jeff.fichtner@gmail.com>
2016-09-05 17:59:53 -0500

    Add first version, *neighbor logic broken*

--- game_controller.rb
+++ game_controller.rb
@@ -16,6 +16,7 @@ class GameController
 
     # alter the board
     game.board = add_glider(game.board)
+    game.board = add_beacon(game.board)
     reset_screen
     GameView.display_board(game.board)
     sleep(0.5)
@@ -34,19 +35,94 @@ class GameController
 
   private
 
-  def self.add_glider(board)
+  def self.add_glider board
     board[3][2] = true
     board[3][3] = true
     board[3][4] = true
     board[2][4] = true
     board[1][3] = true
+    board
   end
 
-  def self.evolve(game)
+  def self.add_beacon board
+    board[10][10] = true
+    board[10][11] = true
+    board[11][10] = true
+    board[12][13] = true
+    board[13][13] = true
+    board[12][12] = true
+    board
+  end
+
+  def self.evolve game
     current_board = game.board
     dup_board = game.board.dup
-    # iterate through current_board
-    # add results to dup_board, return dup_board
+    height = current_board.length - 1
+    width = current_board.first.length - 1
+
+    (0..height).each do |row|
+      (0..width).each do |column|
+        dup_board = check_neighbors(current_board, dup_board, row, column)
+      end
+    end
+    dup_board
   end
 
+  def self.check_neighbors current_board, dup_board, row, column
+    neighbor_count = 0
+
+    if (row > 0 && row < 19 && column > 0 && column < 19)
+      north = current_board[(row - 1)][column]
+      if north
+        neighbor_count += 1
+      end
+
+      northeast = current_board[(row - 1)][(column + 1)]
+      if northeast
+        neighbor_count += 1
+      end
+
+      east = current_board[row][(column + 1)]
+      if east
+        neighbor_count += 1
+      end
+
+      southeast = current_board[(row + 1)][(column + 1)]
+      if southeast
+        neighbor_count += 1
+      end
+
+      south = current_board[(row + 1)][column]
+      if south
+        neighbor_count += 1
+      end
+
+      southwest = current_board[(row + 1)][(column - 1)]
+      if southwest
+        neighbor_count += 1
+      end
+
+      west = current_board[row][(column - 1)]
+      if west
+        neighbor_count += 1
+      end
+
+      northwest = current_board[(row - 1)][(column - 1)]
+      if northwest
+        neighbor_count += 1
+      end
+    end
+
+    if (!current_board[row][column] && neighbor_count == 3)
+      dup_board[row][column] = true
+    elsif current_board[row][column]
+      if (neighbor_count < 2 || neighbor_count > 3)
+        dup_board[row][column] = false
+      elsif (neighbor_count >= 2 && neighbor_count <= 3)
+        dup_board[row][column] = true
+      end
+    end
+
+    dup_board
+  end
 end
