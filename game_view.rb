8b49cf8e0c476b082539705d1e1318c833c0abce
Jeff Fichtner <jeff.fichtner@gmail.com>
2016-09-05 17:02:55 -0500

    Add display board method

--- game_view.rb
+++ game_view.rb
@@ -1,7 +1,23 @@
 class GameView
 
-  def display_board board
-    # display board here
+  def self.display_board board
+    output = String.new
+    height = board.length - 1
+    width = board.first.length - 1
+
+    (0..height).each do |row|
+      (0..width).each do |column|
+        if board[row][column]
+          output << "o"
+        else
+          output << "."
+        end
+      end
+      output << "\n"
+    end
+
+    output << "\n"
+    print output
   end
 
 end
